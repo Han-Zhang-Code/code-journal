03f1aa222bee382fb25ae81ca883a1b0c4ac90df
Yuhan Zhang <zyh514312@gmail.com>
2022-05-31 04:48:22 +0000

    saved to local storage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 var $photourl = document.querySelector('#photo-url');
-
 var $imgHolder = document.querySelector('.adjust-img');
-
 $photourl.addEventListener('input', addImg);
 function addImg(event) {
   $imgHolder.setAttribute('src', event.target.value);
+}
+
+var $submit = document.querySelector('#entry-form');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
+
+$submit.addEventListener('submit', submited);
+function submited(event) {
+  event.preventDefault();
+  var title = $title.value;
+  var notes = $notes.value;
+  var img = $photourl.value;
+  var dataObject = {
+    titleText: title,
+    notesText: notes,
+    imgUrl: img
+  };
+  dataObject.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.push(dataObject);
+  $imgHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $submit.reset();
+}
+
+window.addEventListener('beforeunload', tasks);
+function tasks(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('localStorage', dataJSON);
 }
