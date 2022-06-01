264788d7b92f8ee7d52c35f6c374e5e85f75dfd7
Yuhan Zhang <zyh514312@gmail.com>
2022-06-01 21:52:08 +0000

    swap working, load entry without reload page working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 /* global data */
+if (data.entries.length === 0) {
+  var $createRow = document.createElement('div');
+  $createRow.setAttribute('class', 'row');
+  var $createEmptyContent = document.createElement('div');
+  $createEmptyContent.setAttribute('class', 'set-middle');
+  var $createContent = document.createElement('p');
+  $createContent.textContent = 'No entries have been recorded';
+  var $appendToEntries = document.querySelector('#entries');
+  $appendToEntries.appendChild($createRow);
+  $createRow.appendChild($createEmptyContent);
+  $createEmptyContent.appendChild($createContent);
+}
+
 var $photourl = document.querySelector('#photo-url');
 var $imgHolder = document.querySelector('.adjust-img');
 $photourl.addEventListener('input', addImg);
@@ -26,48 +39,68 @@ function submited(event) {
   data.entries.unshift(dataObject);
   $imgHolder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $submit.reset();
+
+  var $selectContainer = document.querySelector('ul');
+  $selectContainer.prepend(generageEntryDomTree(data.entries[0]));
 }
 
-/* <div class="row">
-  <div class="column-half">
-    <img src="images/placeholder-image-square.jpg" alt="placeholder-image-square" class="adjust-img column-full"">
-  </div>
-  <div class="column-half">
-    <ul>
-      <li class="list-title">title</li>
-      <li class="list-content">To be clear, let's reiterate what is happening here. The element we've given a display value of flex to is acting like a
-        block-level element in terms of how it interacts with the rest of the page, but its children are laid out as flex items.
-        The next section will explain in more detail what this means. Note also that you can use a display value of inline-flex
-        if you wish to lay out an element's children as flex items, but have that element behave like an inline element.</li>
-    </ul>
-  </div>
-</div> */
+window.addEventListener('DOMContentLoaded', loadDomTree);
+function generageEntryDomTree(entry) {
+  var $createList = document.createElement('li');
+  var $createRow = document.createElement('div');
+  $createRow.setAttribute('class', 'row');
+  var $createColumn = document.createElement('div');
+  $createColumn.setAttribute('class', 'column-half');
+  var $createImg = document.createElement('img');
+  $createImg.setAttribute('src', entry.imgUrl);
+  $createImg.setAttribute('class', 'adjust-img column-full');
+  var $create2Column = document.createElement('div');
+  $create2Column.setAttribute('class', 'column-half');
+  var $createTitlediv = document.createElement('div');
+  $createTitlediv.setAttribute('class', 'list-title');
+  $createTitlediv.textContent = entry.titleText;
+  var $createContentdiv = document.createElement('div');
+  $createContentdiv.setAttribute('class', 'list-content');
+  $createContentdiv.textContent = entry.notesText;
 
-// window.addEventListener('DOMContentLoaded', domCreation);
-// function domCreation(datas) {
-//   console.log(datas);
-//   var $createRow = document.createElement('div');
-//   $createRow.setAttribute('class', 'row');
-//   var $createColumn = document.createElement('div');
-//   $createColumn.setAttribute('class', 'column-half');
-//   var $createImg = document.createElement('img');
-//   $createImg.setAttribute('src', datas.entries[0].imgUrl);
-//   var $create2Column = document.createElement('div');
-//   $create2Column.setAttribute('class', 'column-half');
-//   var $createUnorderedList = document.createElement('ul');
-//   var $createTitleList = document.createElement('li');
-//   $createTitleList.setAttribute('class', 'list-title');
-//   $createTitleList.textContent = datas.entries[0].titleText;
-//   var $createContentList = document.createElement('li');
-//   $createContentList.setAttribute('class', 'list-content');
-//   $createContentList.textContent = datas.entries.notesText;
+  $createList.appendChild($createRow);
+  $createRow.appendChild($createColumn);
+  $createColumn.appendChild($createImg);
+  $createRow.appendChild($create2Column);
+  $create2Column.appendChild($createTitlediv);
+  $create2Column.appendChild($createContentdiv);
 
-//   $createRow.appendChild($createColumn);
-//   $createColumn.appendChild($createImg);
-//   $createRow.appendChild($create2Column);
-//   $create2Column.appendChild($createUnorderedList);
-//   $createUnorderedList.appendChild($createTitleList);
-//   $createUnorderedList.appendChild($createContentList);
+  return $createList;
+}
+function loadDomTree(event) {
+  var $selectContainer = document.querySelector('ul');
+  for (var i = 0; i < data.entries.length; i++) {
+    var $appendToview = generageEntryDomTree(data.entries[i]);
+    $selectContainer.appendChild($appendToview);
+  }
+}
 
-//   return $createRow;
-// }
+var $views = document.querySelectorAll('.view');
+var $entriesNavButton = document.querySelector('.header-entries');
+$entriesNavButton.addEventListener('click', viewEntries);
+function viewEntries(event) {
+  for (var i = 0; i < $views.length; i++) {
+    if ($views[i].getAttribute('data-view') === 'entry-form') {
+      $views[i].className = 'hidden';
+    } else if ($views[i].getAttribute('data-view') !== 'entry-form') {
+      $views[i].className = 'view';
+    }
+  }
+}
+
+var $anchor = document.querySelector('#anchor');
+$anchor.addEventListener('click', toNewEntry);
+function toNewEntry(event) {
+  for (var i = 0; i < $views.length; i++) {
+    if ($views[i].getAttribute('data-view') === 'entry-form') {
+      $views[i].className = 'view';
+    } else if ($views[i].getAttribute('data-view') !== 'entry-form') {
+      $views[i].className = 'hidden';
+    }
+  }
+}
